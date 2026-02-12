a82cc30facaf7dec87a59a83f92884d5175cf47d
Haimanot515 <haimanotbeka@gmail.com>
2026-02-12 10:32:22 +0300

    Update skills.js

--- backend/routes/skills.js
+++ backend/routes/skills.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const auth = require("../middleware/authMiddleware");
 const adminAuth = require("../middleware/adminMiddleware");
 const { getSkills, createSkill } = require("../controllers/skillController");
+/* IMPORT MULTER MIDDLEWARE */
+const upload = require("../middleware/multerMiddleware"); // Ensure this path matches your file structure
 
-router.get("/",auth, getSkills);
-router.post("/", auth,adminAuth, createSkill);
+router.get("/", auth, getSkills);
+
+/* ADDED upload.single("image") TO PARSE THE FORMDATA */
+router.post("/", auth, adminAuth, upload.single("image"), createSkill);
 
 module.exports = router;
